d7142818ccf5edf5ae5727ddd5b3d0c049e6e02f
Vladimir Shevelevich <vshevelevich@playmous.com>
2025-11-14 11:14:43 +0100

    add boot and gamescope

---
+++ Assets/App/Scripts/Core/GameScope.cs
@@ -0,0 +1,13 @@
+﻿using VContainer;
+using VContainer.Unity;
+
+namespace App.Core
+{
+    public class GameScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+            
+        }
+    }
+}
